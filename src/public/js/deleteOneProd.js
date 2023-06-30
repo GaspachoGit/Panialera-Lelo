87075e72056e695bd9027c985e2b52004aafb0f3
GaspachoGit <gaston2.0409@gmail.com>
2023-06-30 10:33:44 -0300

    add feature: confirmation alert (sweetAlert)

--- src/public/js/deleteOneProd.js
+++ src/public/js/deleteOneProd.js
@@ -1,4 +1,55 @@
-const btnDelete = document.querySelectorAll('.btn-add-to-cart');
+/* const Swal = require('sweetalert2');*/
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btnDelete = document.querySelectorAll('.btn-delete');
+
+  btnDelete.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const productId = event.target.dataset.productId;
+
+      const url = `http://localhost:8080/api/products/${productId}`;
+
+      // Mostrar alerta de confirmación
+      Swal.fire({
+        title: '¿Estás seguro?',
+        text: 'Esta acción eliminará el producto',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(url, {
+            method: 'DELETE'
+          })
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Error al eliminar el producto');
+              }
+              // Producto eliminado correctamente
+              Swal.fire('¡Eliminado!', 'El producto ha sido eliminado.', 'success')
+                .then(() => {
+                  // Actualizar la página
+                  location.reload();
+                });
+            })
+            .catch(error => {
+              console.error(error);
+              Swal.fire('Error', 'Se produjo un error al eliminar el producto.', 'error');
+            });
+        }
+      });
+    });
+  });
+});
+
+
+
+
+/* const btnDelete = document.querySelectorAll('.btn-add-to-cart');
 
 btnDelete.forEach((btn) => {
   btn.addEventListener('click', (event) => {
@@ -21,4 +72,4 @@ btnDelete.forEach((btn) => {
         console.error(error);
       });
   });
-});
+}); */
