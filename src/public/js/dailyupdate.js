3b719bbfa162b93e707725670b41f2f1642ad6fd
GaspachoGit <gaston2.0409@gmail.com>
2023-06-30 15:39:41 -0300

    add feature, sweet alert confirmation

--- src/public/js/dailyupdate.js
+++ src/public/js/dailyupdate.js
@@ -3,14 +3,27 @@ document.getElementById('closeDailyBtn').addEventListener('click', function(even
   const url = `http://localhost:8080/api/box/${bid}/daily`;
   const method = 'PATCH';
   
-  fetch(url, { method })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-			location.reload();
-    })
-    .catch(error => {
-      console.error(error);
-      // Manejar el error de la solicitud, mostrar mensaje de error, etc.
-    });
+  Swal.fire({
+    title: 'Cerrar caja',
+    text: '¿Estás seguro de cerrar la caja?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Sí, cerrar',
+    cancelButtonText: 'Cancelar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      fetch(url, { method })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          Swal.fire('Caja cerrada', 'La caja ha sido cerrada exitosamente', 'success').then(() => {
+            location.reload();
+          });
+        })
+        .catch(error => {
+          console.error(error);
+          Swal.fire('Error', 'Ocurrió un error al cerrar la caja', 'error');
+        });
+    }
+  });
 });
--- src/public/js/deleteOneProd.js
+++ src/public/js/deleteOneProd.js
@@ -1,6 +1,3 @@
-/* const Swal = require('sweetalert2');*/
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const btnDelete = document.querySelectorAll('.btn-delete');
 
@@ -46,30 +43,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-
-
-
-/* const btnDelete = document.querySelectorAll('.btn-add-to-cart');
-
-btnDelete.forEach((btn) => {
-  btn.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    const productId = event.target.dataset.productId;
-
-    const url = `http://localhost:8080/api/products/${productId}`;
-
-    fetch(url, {
-      method: 'DELETE'
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Error al eliminar el producto');
-        }
-        console.log('Producto eliminado');
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  });
-}); */
